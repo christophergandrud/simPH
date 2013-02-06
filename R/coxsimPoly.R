f4605d2d8ec08c295f0fa97288d12797d9765ebd
Christopher Gandrud <christopher.gandrud@gmail.com>
2013-02-06 13:02:06 +0900

    multi-poly

--- R/coxsimPoly.R
+++ R/coxsimPoly.R
@@ -1,8 +1,11 @@
 #' Simulate non-linear hazards for a range of values
 #'
 #' \code{coxsimPoly} simulates hazards for polynomial covariate effects.
-#' @param obj a coxph fitted model object with a polynomial coefficient. 
-#' 
+#' @param obj a coxph fitted model object with a polynomial coefficient.
+#' @param b character string name of the coefficient you would like to simulate.
+#' @param pow numeric polynomial used in \code{coxph}.  
+#' @param X numeric vector of values of X to simulate relative hazards for.
+#' @param nsim the number of simulations to run per value of X. Default is \code{nsim = 1000}.
 #' @description Simulates hazards for polynomial covariate effects.
 #' 
 #' @examples
@@ -12,6 +15,12 @@
 #' # Load survival package
 #' library(survival)
 #'
+#' # Run basic model
+#' M1 <- coxph(Surv(acttime, censor) ~ prevgenx + lethal + deathrt1 + 
+#'              acutediz + hosp01  + hhosleng + mandiz01 + femdiz01 + 
+#'              peddiz01 + orphdum + natreg + I(natreg^2) + vandavg3 + 
+#'              wpnoavg3 + condavg3 + orderent + stafcder, 
+#'             data = CarpenterFdaData)
 #'
 #' @references Keele, Luke. 2010. “Proportionally Difficult: Testing for Nonproportional Hazards in Cox Models.” Political Analysis 18(2): 189–205.
 #'
@@ -19,14 +28,26 @@
 #' @import MSBVAR plyr reshape2 survival
 #' @export 
 
-coxsimPoly <- function(obj, b, pow, from, to, by) 
+coxsimPoly <- function(obj, b, pow = 2, X = 1, nsim = 1000) 
 {
-
 	Coef <- matrix(obj$coefficients)
 	VC <- vcov(obj)
 	    
 	Drawn <- rmultnorm(n = nsim, mu = Coef, vmat = VC)
 	DrawnDF <- data.frame(Drawn)
+	dfn <- names(DrawnDF)
 
-	bi <- paste0("I(", b, "^", pow, ")")
+	bpos <- match(b, dfn)
+
+	NamesLoc <- function(p){
+		Temp <- paste0("I.", b, ".", p, ".")
+		match(Temp, dfn)
+  	}
+	pows <- 2:pow
+  	NamePow <- sapply(pows, NamesLoc, simplify = TRUE)
+
+	NamePow <- c(bpos, NamePow)
+  	Drawn <- data.frame(Drawn[, NamePow])
+  	Drawn$ID <- 1:nsim
+
 }
